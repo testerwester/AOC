74f326ddf10a484f620feae7faae128c8122e962
Christoffer <krillebjoerk@live.se>
2020-08-16 17:54:25 +0200

    Working AOC_pt2

--- AOC2/AOC2/Program.cs
+++ AOC2/AOC2/Program.cs
@@ -5,41 +5,95 @@ namespace AOC2
 {
     class Program
     {
-        /*  Constant used for Program counter   */
-        public const int OPLENGTH = 4;
+        /*  Constants   */
+        public const int GOAL_VAL = 19690720;
+        public const int BUFF_SIZE = 1024;
+
+        class OpCode
+        {
+            public const int OPLENGTH = 4;
+            public const int ADD = 1;
+            public const int MULT = 2;
+            public const int END = 99;
+        }
 
         static void Main(string[] args)
         {
 
             string[] values = CsvToArray("AOCInput2.txt");
             int[] opcodes = StrToInt(values);
-
-            /*  Manually change index 1 and 2 according to task description */
-            opcodes[1] = 12; 
-            opcodes[2] = 2;
-            Console.WriteLine("----------------------------------------------------");
-            RunProgram(opcodes);
-
-             
             
+
+            if(values != null && opcodes != null)
+            {
+                /*  Part one    */
+                RunPartOne(opcodes);
+
+                /* Part Two */
+                RunPartTwo(opcodes);
+            }
+            else
+            {
+                Console.WriteLine("Unable to run program.");
+            }
+
+
+
 
         }
 
-
-        private static void RunProgram(int[] ops)
+        private static void RunPartOne(int[] opcodes)
         {
-            int PC = 0; //ProgramCounter for opcodes
+            Console.WriteLine("------------------------------------------------------------");
+            Console.WriteLine($"1. Value at 0 is: {RunProgram(opcodes, 12, 2)}");
+            Console.WriteLine("------------------------------------------------------------");
+        }
 
-            /*  Runs instructions as long as opcodes are accepted by runOP   */
-            while(runOP(PC, ops))
+        private static void RunPartTwo(int[] opcodes)
+        {
+            bool foundVal = false;
+
+            for (int noun = 0; noun < 100; noun++)
             {
-                PC += OPLENGTH;
+                for (int verb = 0; verb < 100; verb++)
+                {
+                    if (RunProgram(opcodes, noun, verb) == GOAL_VAL)
+                    {
+                        foundVal = true;
+                        Console.WriteLine($"2. Noun: {noun}, Verb: {verb} where (100 * noun + verb) = {100 * noun + verb}");
+                        break;
+                    }
+                }
             }
 
-            /*  End output to test program */
-            Console.WriteLine("----------------------------------------------------");
-            Console.WriteLine($"Value at position 0 is: {ops[0]}");
-            Console.WriteLine("----------------------------------------------------");
+            if (!foundVal)
+            {
+                Console.WriteLine($"No value found that corresponds to {GOAL_VAL}");
+            }
+            Console.WriteLine("------------------------------------------------------------");
+        }
+
+
+        private static int RunProgram(int[] indata, int noun, int verb)
+        {
+            int PC = 0; //ProgramCounter for opcodes
+            int[] ops = new int[indata.Length];
+
+            /*  Copies values for to temp array */
+            for (int i = 0; i < indata.Length; i++) ops[i] = indata[i]; 
+
+            /*  Adds noun and verb on correct spot in instructions */
+            ops[1] = noun; 
+            ops[2] = verb;
+
+
+            /*  Runs instructions as long as opcodes are accepted by runOP   */
+            while (runOP(PC, ops))
+            {
+                PC += OpCode.OPLENGTH;
+            }
+
+            return ops[0];
         }
 
         private static bool runOP(int PC, int[] opArr)
@@ -48,26 +102,22 @@ namespace AOC2
             int oPTwoIndex = opArr[PC + 2];
             int destIndex = opArr[PC + 3];
 
-            Console.Write($"PC {PC}: ");
-
             switch(opArr[PC])
             {
-                case 1:
-                    Console.WriteLine($"At index {opArr[(PC + 3)]} setting {opArr[(PC + 1)]} + {opArr[(PC + 2)]}");
+                case OpCode.ADD:
                     opArr[destIndex] = opArr[opOneIndex] + opArr[oPTwoIndex];
                     return true;
 
-                case 2:
-                    Console.WriteLine($"At index {opArr[(PC + 3)]} setting {opArr[(PC + 1)]} * {opArr[(PC + 2)]}");
+                case OpCode.MULT:
                     opArr[destIndex] = opArr[opOneIndex] * opArr[oPTwoIndex];
                     return true;
 
-                case 99:
-                    Console.WriteLine($"Instruction END");
+                /*  For troubleshooting/extension   */
+                case OpCode.END: 
                     return false;
 
+                /*  catches all "unknown op codes"  */
                 default:
-                    Console.WriteLine($"Unknown OP code");
                     return false;
             }
 
@@ -75,41 +125,33 @@ namespace AOC2
 
 
 
-        private static int[] StrToInt(string[] arr)
+        private static int[] StrToInt(string[] stringCodes)
         {
-            int[] intArr = new int[arr.Length];
+            int[] intArr = new int[stringCodes.Length];
 
-            for(int i = 0; i<arr.Length; i++)
+            for(int i = 0; i<stringCodes.Length; i++)
             {
-                intArr[i] = Int32.Parse(arr[i]);
+                intArr[i] = Int32.Parse(stringCodes[i]);
             }
-
             return intArr;
         }
 
         /*  Extracts values from a CSV file, removes commas and return an array of values */
         private static string[] CsvToArray(string filename)
         {
-
             try
             {
                 using (StreamReader sr = new StreamReader(filename))
                 {
                     string read;
-                    string[] characters = new string[1024];
+                    string[] characters = new string[BUFF_SIZE];
 
                     while((read = sr.ReadLine()) != null)
                     {
                         characters = read.Split(",");
                     }
-
-
-                    //VAD HÄNDER OM MER ÄN 1024 tecken?????????+
-                    //Bygg någon kontrollfunktion
                     return characters;
                 }
-
-                    
             }
 
             catch(IOException e)
